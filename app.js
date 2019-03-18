5b01c4624d7f424e309aa551aefa01b2f11ae51d
arjunha93 <48612246+arjunha93@users.noreply.github.com>
2019-03-18 14:41:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-var arr = [12,43,56];
- var max = arr[0];
- for(var i=0;i<arr.length;i++){
-     if(arr[i] > max){
-         max=arr[i];
-     }
- }
- 
- console.log('max : ' + max);
+var arr = [
+  {
+	name: 'arjun',
+	age: 26,
+	salary: "9lpa",
+	city: "mandya",
+	state: "karnataka",
+	pincode: 560103
+},{
+	name: 'akshay',
+	age: 29,
+	salary: "8lpa",
+	city: "bhuvwneshwar",
+	state: "Odisa",
+	pincode: 560102
+}, {
+	name: "nutan",
+	age: 29,
+	salary: "10lpa",
+	city: "Banglore",
+	state: "Uttar Pradesh",
+	pincode: 560104
+}, {
+	name: "pratik",
+	age: 26,
+	salary: "4lpa",
+	city: "solapur",
+	state: "Maharashtra",
+	pincode: 513224
+}];
 
-var first = 17;
-var second = 84;
-var third = 41;
-
-console.log(first > second ? first : second > third ? second : third);
+for(var obj in arr){
+    console.log(" name : "+arr[obj].name+"\n age : "  + arr[obj].age+"\n salary : " + arr[obj].salary+"\n city : " + arr[obj].city +"\n state : "+ arr[obj].state+"\n pincode : " + arr[obj].pincode);
+}
